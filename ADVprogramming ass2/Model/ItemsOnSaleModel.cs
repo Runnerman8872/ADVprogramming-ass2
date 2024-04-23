8751f6918574964bb092120dafd9c03608a23631
Runnerman8872 <STE22002958@btc.ac.uk>
2024-04-23 14:58:37 +0100

    Finalised tables

--- ADVprogramming ass2/Model/ItemsOnSaleModel.cs
+++ ADVprogramming ass2/Model/ItemsOnSaleModel.cs
@@ -2,19 +2,32 @@
 //update-database
 
 using System.ComponentModel.DataAnnotations;
+using ADVprogramming_ass2.Model;
 
 namespace ADVprogramming_ass2.Model
 {
     public class ItemsOnSaleModel
     {
         [Key]
-        public int Id { get; set; }
+        public int Item_Id { get; set; }
 
         [Required]
-        public string Name { get; set; }
+        [StringLength(75)]
+        public string Item_Name { get; set; }
+
+        [Required]
+        [StringLength(75)]
         public string Item_Type { get; set; }
+
+        [Required]
+        [StringLength(75)]
         public string Supplier { get; set; }
+
+        [Required]
         public int Amnt_left { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
         public float Price_Pound { get; set; }
     }
 }
--- ADVprogramming ass2/Model/UserModel.cs
+++ ADVprogramming ass2/Model/UserModel.cs
@@ -1,6 +1,28 @@
-﻿namespace ADVprogramming_ass2.Model
+﻿//add-migration AddingItemModel
+//update-database
+
+using System.ComponentModel.DataAnnotations;
+using ADVprogramming_ass2.Model;
+
+namespace ADVprogramming_ass2.Model
 {
     public class UserModel
     {
+        [Key]
+        public int User_Id { get; set; }
+
+        [Required]
+        [StringLength(75)]
+        public string User_Name { get; set;}
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string User_Email { get; set;}
+
+        [Required]
+        public bool Loyalty_Card { get; set; }
+
+
     }
 }
