47fc9a899f69388bdaac0ffdab4f7ca2554bc82a
Runnerman8872 <STE22002958@btc.ac.uk>
2024-04-25 13:50:19 +0100

    added screen for future item database interaction

---
+++ ADVprogramming ass2/Pages/Item_DB_Screen.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ADVprogramming_ass2.Pages
+{
+    public class Item_DB_ScreenModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
