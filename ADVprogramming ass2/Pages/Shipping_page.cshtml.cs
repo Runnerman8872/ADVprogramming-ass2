2170e249891b112bc63d6bdfa4145d8583d4a797
Runnerman8872 <STE22002958@btc.ac.uk>
2024-04-25 14:55:53 +0100

    added more pages

---
+++ ADVprogramming ass2/Pages/Shipping_page.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ADVprogramming_ass2.Pages
+{
+    public class Shipping_pageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
