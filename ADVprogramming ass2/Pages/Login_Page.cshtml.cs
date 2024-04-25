4f7695fcdae15f7e8110f85c9f1085e6e6cdda7f
Runnerman8872 <STE22002958@btc.ac.uk>
2024-04-25 14:52:12 +0100

    Added more pages

---
+++ ADVprogramming ass2/Pages/Login_Page.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ADVprogramming_ass2.Pages
+{
+    public class Login_PageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ADVprogramming ass2/Pages/Shop_page.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ADVprogramming_ass2.Pages
+{
+    public class Shop_pageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
