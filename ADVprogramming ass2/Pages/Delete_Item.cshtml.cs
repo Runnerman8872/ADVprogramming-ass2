d7ddbf24df837db59361a37a0c38cdf7faef000f
Runnerman8872 <STE22002958@btc.ac.uk>
2024-04-30 17:32:10 +0100

    Users can now be deleted

--- ADVprogramming ass2/Pages/Delete_Item.cshtml.cs
+++ ADVprogramming ass2/Pages/Delete_Item.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using ADVprogramming_ass2.Model;
 using System.Threading.Tasks;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 
 namespace ADVprogramming_ass2.Pages
 {
@@ -21,12 +22,23 @@ namespace ADVprogramming_ass2.Pages
         public void OnGet(Guid id)
         {
             Items = _dbConnection.Item.FirstOrDefault(t => t.Item_Id == id);
-        
+            Users = _dbConnection.User_Name.FirstOrDefault(t => t.User_Id == id);
         }
 
         public async Task<IActionResult> OnPostAsync(Guid id)
         {
             var itemToDelete = _dbConnection.Item.FirstOrDefault(t => t.Item_Id == id);
+            var userToDelete = _dbConnection.User_Name.FirstOrDefault(t => t.User_Id == id);
+
+
+            if (userToDelete != null)
+            {
+                _dbConnection.User_Name.Remove(userToDelete);
+
+                await _dbConnection.SaveChangesAsync();
+
+                return RedirectToPage("User_Database");
+            }
 
             if (itemToDelete != null)
             {
@@ -36,6 +48,7 @@ namespace ADVprogramming_ass2.Pages
 
                 return RedirectToPage("Item_Database");
             }
+
             else
             {
                 return NotFound();
